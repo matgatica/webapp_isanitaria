f6feacd8551e823227690658d2ea30d3861333e6
matgatica <70600924+matgatica@users.noreply.github.com>
2021-12-13 14:02:50 -0300

    act

--- apps/app4.py
+++ apps/app4.py
@@ -8,6 +8,27 @@ import plotly.express as px
 import pandas as pd
 from datetime import date
 
+
+pcr_por_region=pd.read_csv("https://raw.githubusercontent.com/MinCiencia/Datos-COVID19/master/output/producto7/PCR.csv")
+pcr_unpivoted = pcr_por_region.melt(id_vars=['Region'], var_name='date', value_name='cuenta')
+lista_regiones_pcr=[]
+for i in pcr_por_region.Region.unique():
+    mini_dict={'label':f'{i}',"value":f"{i}"}
+    lista_regiones_pcr.append(mini_dict)
+
+totales_por_region=pd.read_csv("https://raw.githubusercontent.com/MinCiencia/Datos-COVID19/master/output/producto3/TotalesPorRegion.csv")
+totales_unpivoted = totales_por_region.melt(id_vars=['Region','Categoria'], var_name='date', value_name='cuenta')
+lista_regiones_totales=[]
+for i in totales_por_region.Region.unique():
+    mini_dict={'label':f'{i}',"value":f"{i}"}
+    lista_regiones_totales.append(mini_dict)
+
+lista_categorias_totales=[]
+for i in totales_por_region.Categoria.unique():
+    mini_dict={'label':f'{i}',"value":f"{i}"}
+    lista_categorias_totales.append(mini_dict)
+
+
 ventiladores=pd.read_csv("DATA/ventiladores_nacional.csv",sep=',').drop(columns=["Unnamed: 0"])
 ventiladores_unpivoted = ventiladores.melt(id_vars=['Ventiladores'], var_name='date', value_name='cuenta')
 lista_ventiladores=[
@@ -82,6 +103,61 @@ sidebar = html.Div(
     style=SIDEBAR_STYLE,
 )   
 
+
+
+content_pcr = dbc.Row(
+    [
+        
+        html.H2(children='PCR NACIONAL'),
+        dcc.Graph(id='pcr_nacional'),
+
+        dcc.Dropdown(
+            id='id_regiones_pcr',
+            options=lista_regiones_pcr,
+            value=['Metropolitana'],
+            multi=True     
+        ),
+        dcc.DatePickerRange(
+            id='date_range_pcr',
+            min_date_allowed=date(1995, 8, 5),
+            max_date_allowed=date(2022, 1, 1),
+            initial_visible_month=date(2020, 6, 1),
+            end_date=date(2021, 12 , 1),
+            start_date=date(2020, 6, 1)
+        )]
+        )
+
+content_totales = dbc.Row(
+    [
+        
+        html.H2(children='TOTALES NACIONAL'),
+        dcc.Graph(id='totales_nacional'),
+
+        dcc.Dropdown(
+            id='id_regiones_totales',
+            options=lista_regiones_totales,
+            value=['Metropolitana'],
+            multi=True     
+        ),
+        dcc.Dropdown(
+            id='id_categorias_totales',
+            options=lista_categorias_totales,
+            value='Casos acumulados',
+               
+        ),
+        dcc.DatePickerRange(
+            id='date_range_totales',
+            min_date_allowed=date(1995, 8, 5),
+            max_date_allowed=date(2022, 1, 1),
+            initial_visible_month=date(2020, 6, 1),
+            end_date=date(2021, 12 , 1),
+            start_date=date(2020, 6, 1)
+        )]
+        )
+
+
+
+
 content_ventiladores = dbc.Row(
     [
         
@@ -139,6 +215,8 @@ content = html.Div(
         html.H2('INFO NACIONAL', style=TEXT_STYLE),
         html.H6('<Inteligencia Sanitaria>', style=TEXT_STYLE),
         html.Hr(),
+        content_totales,
+        content_pcr,
         content_ventiladores,
         content_residencias
     
@@ -148,6 +226,61 @@ content = html.Div(
 )
 
 layout = html.Div([sidebar, content])
+
+
+
+@app.callback(
+    Output('pcr_nacional', 'figure'),
+    [
+    Input('id_regiones_pcr', 'value'),
+    Input('date_range_pcr', 'start_date'),
+    Input('date_range_pcr', 'end_date'),
+    ]
+    )
+
+def update_graph(regiones_pcr,startdate,enddate):               
+    df=pcr_unpivoted
+    
+    df=df[(df.Region.isin(regiones_pcr))]
+    dff = df[(df.date>=startdate)&(df.date<=enddate)]
+    fig3 = px.line(dff,x='date',y='cuenta',color='Region',template='plotly_dark')
+
+    fig3.update_layout(margin={'l': 40, 'b': 40, 't': 10, 'r': 0}, hovermode='closest')
+
+    return fig3
+
+
+
+
+
+
+
+
+
+
+@app.callback(
+    Output('totales_nacional', 'figure'),
+    [
+    Input('id_regiones_totales', 'value'),
+    Input('id_categorias_totales', 'value'),
+    Input('date_range_totales', 'start_date'),
+    Input('date_range_totales', 'end_date'),
+    ]
+    )
+
+def update_graph(regiones_totales,categorias_totales,startdate,enddate):               
+    df=totales_unpivoted
+    
+    df=df[(df.Region.isin(regiones_totales))&(df.Categoria==categorias_totales)]
+    dff = df[(df.date>=startdate)&(df.date<=enddate)]
+    fig3 = px.line(dff,x='date',y='cuenta',color='Region',template='plotly_dark')
+
+    fig3.update_layout(margin={'l': 40, 'b': 40, 't': 10, 'r': 0}, hovermode='closest')
+
+    return fig3
+
+
+
 
 
 @app.callback(
