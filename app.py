075c7baacd7b569061a0d7ba1e0df34ec3e7fb68
matgatica <70600924+matgatica@users.noreply.github.com>
2022-01-05 09:49:44 -0300

    act

--- app.py
+++ app.py
@@ -2,5 +2,8 @@
 import dash
 import dash_bootstrap_components as dbc
 
-app = dash.Dash(__name__, suppress_callback_exceptions=True,external_stylesheets=[dbc.themes.CYBORG])
+app = dash.Dash(__name__, suppress_callback_exceptions=True,external_stylesheets=[dbc.themes.CYBORG],
+meta_tags=[
+        {"name": "viewport", "content": "width=device-width, initial-scale=1"}
+    ],)
 server = app.server
